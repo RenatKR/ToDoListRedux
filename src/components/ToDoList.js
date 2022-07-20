645e933db646a90fe543a254c3cd6d01bcd1f70b
Kurbangaliev Renat <kurbangaliev1987@mail.ru>
2022-07-20 16:25:24 +0300

    2072022

---
+++ src/components/ToDoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ToDoList(props) {
+    return (
+        <>
+            <div>ToDoList</div>
+            <div>{props.text}</div>
+        </>
+
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const initialState = {
+  amount: 0
+}
+
+const reducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case 'AMOUNT':
+      return { ...state, amount: state.amount + 1 }
+    default: return state;
+  }
+}
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider >,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
